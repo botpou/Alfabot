e9b00fe88b2d27606fd988bc31d2361bdcfa9d5b
andymrlit <andysebastien14@gmail.com>
2022-10-01 09:49:31 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,14 +14,14 @@ global.APIKeys = {
 
 // setting 
 global.autoread = true // auto read message
-global.autobio = true 
-global.userRegister = false 
+global.autobio = true
+global.userRegister = true
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
+global.language = english  //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
